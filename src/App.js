2a0a5b0c7cf39339ce20bd4b8ae45ceb0699ac0f
Olga Rad <olgaradeeva5947@gmail.com>
2021-06-21 06:59:22 +0300

    day2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import {addCustomerAction, removeCustomerAction} from './store/customer_reducer';
+import { fetchCustomers } from './asyncAction/customers';
 
 function App() {
   const dispatch = useDispatch()
   const cash = useSelector(state => state.cash.cash)
   const customers = useSelector(state => state.customers.customers)
   console.log(cash)
+
+  useEffect(()=>{},[])
 
   const addCash = (cash) => {
     dispatch({ type: "ADD_CASH", payload: cash })
@@ -17,11 +22,15 @@ function App() {
   }
 
   const addCustomer = (name) => {
-    const customer ={
+    const customer = {
       name,
       id: Date.now(),
     }
-    dispatch({ type: "ADD_CUSTOMER", payload: customer })
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
   }
 
   return (
@@ -32,14 +41,20 @@ function App() {
         <button onClick={() => getCash(Number(prompt()))}>get money</button>
         <button onClick={() => addCustomer(prompt())}>add client</button>
         <button onClick={() => getCash(Number(prompt()))}>delete client</button>
+        <button onClick={() => dispatch(fetchCustomers())}>add clientss</button>
       </div>
       {customers.length > 0 ?
         <div>
           {customers.map(customer =>
-            <div> {customer.name})</div>)}
-        </div> 
+            <div
+              onClick={() => removeCustomer(customer)}
+              style={{ fontSize: "2rem", border: '1px solid black', padding: "10px", marginTop: 5 }}
+            >
+              {customer.name}
+            </div>)}
+        </div>
         :
-        <div style={{fontSize:"2rem"}}>
+        <div style={{ fontSize: "2rem" }}>
           no clients!
         </div>
       }
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
-import {createStore, combineReducers} from 'redux'
+import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {cashReducer} from './cash_reducer'
 import {customerReducer} from './customer_reducer'
 import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
     cash: cashReducer, //cash - ключ значение, для сокращенного использования имени
     customers: customerReducer,
 })
 
-export const store = createStore(rootReducer, composeWithDevTools())
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
